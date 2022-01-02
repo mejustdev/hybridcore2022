b49c11b8aebe97d2e876084ec4e2b67136222238
Me <mergorgec@gmail.com>
2022-01-02 18:53:22 +0100

    add update

---
+++ studio/resolveProductionUrl.js
@@ -0,0 +1,20 @@
+// ./studio/resolveProductionUrl.js
+
+// Any random string, must match SANITY_PREVIEW_SECRET in the Next.js .env.local file
+const previewSecret = 'abc890'
+
+// Replace `remoteUrl` with your deployed Next.js site
+const remoteUrl = `https://sanity-io-next-js-blog-one.vercel.app`
+const localUrl = `http://localhost:3000`
+
+export default function resolveProductionUrl(doc) {
+  const baseUrl = window.location.hostname === 'localhost' ? localUrl : remoteUrl
+
+  const previewUrl = new URL(baseUrl)
+
+  previewUrl.pathname = `/api/preview`
+  previewUrl.searchParams.append(`secret`, previewSecret)
+  previewUrl.searchParams.append(`slug`, doc?.slug?.current ?? `/`)
+
+  return previewUrl.toString()
+}

d50bf57ff4fbce00e424675112c1b46453dcf574
Me <mergorgec@gmail.com>
2022-01-04 23:08:06 +0100

    add update

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -2,13 +2,14 @@
 const remoteUrl = `https://sanity-io-next-js-blog-one.vercel.app`
 const localUrl = `http://localhost:3000`
 
+const previewSecret = "OXCeU69ElOP14pTRu6JWQ3KxVtEanL"
 export default function resolveProductionUrl(doc) {
   const baseUrl = window.location.hostname === 'localhost' ? localUrl : remoteUrl
 
   const previewUrl = new URL(baseUrl)
 
   previewUrl.pathname = `/api/preview`
-  previewUrl.searchParams.append(`secret`, process.env.SANITY_PREVIEW_SECRET)
+  previewUrl.searchParams.append(`secret`, previewSecret)
   previewUrl.searchParams.append(`slug`, doc?.slug?.current ?? `/`)
 
   return previewUrl.toString()
